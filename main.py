3e9e00c0261bc704d90697af74ec77af9c1cc04c
lucgutz <118909534+lucgutz@users.noreply.github.com>
2022-12-20 16:46:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,64 @@
 # Do not modify these lines
-__winc_id__ = '499e67d5cb54448e93cee7465be2c866'
-__human_name__ = 'calculate'
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
 
 # Add your code after this line
-broccoli = 2
-leek = 2
-potato = 3
-brussel_sprout = 7
+#part 1
+#1
+player_goal_one = "Ruud Gullit"
+player_goal_two = "Marco van Basten"
 
-sum_one_each = float((broccoli+leek+potato+brussel_sprout))
-print (sum_one_each)
+#2
+goal_0 = 32
+goal_1 = 54
 
-avg_price = float((sum_one_each/4))
-print (avg_price)
+#3
+scorers = player_goal_one + " " + str(goal_0) + ", " + player_goal_two + " " + str(goal_1)
+print(scorers)
 
-num_potatoes = 7
-num_broccolis = 5
-num_leeks = 2
-num_brussel_sprouts = 10
+scorers_fstring = f"{player_goal_one} {goal_0}, {player_goal_two} {goal_1}"
+print(scorers_fstring)
 
-sum_total = float((num_potatoes*potato)+(num_broccolis*broccoli)+(num_leeks*leek)+(num_brussel_sprouts*brussel_sprout))
-print (sum_total)
+#4
+report = f"{player_goal_one} scored in the {goal_0}nd minute\n{player_goal_two} scored in the {goal_1}th minute"
+print(report)
 
-discount_percentage = 30
+#part 2
+#1
+player = "Hans van Breukelen"
 
-discounted_sum_total = float((100-discount_percentage)*(sum_total)/100)
-print (discounted_sum_total)
+#2
+find_space = player.find(" ")
+first_name = player[:find_space]
+print(first_name)
 
+#3
+#find option is: valuable.find(value, start, end)
+find_start = player.find("v")
+last_name = player[find_start:]
+print(last_name)
 
+last_name_len = len(last_name)
+print(last_name_len)
 
+#4
+name_short = player.replace("Hans","H.")
+print(name_short)
 
+#5
+first_name_len = len(first_name)
+chant = f"{first_name}! " *first_name_len #hier is laatste karakter een space
+chant = chant[:-1] #laatste space eraf slicen
+print(chant)
+
+#6
+last_character = chant[-1]
+good_chant = last_character != " "
+print(good_chant)
+
+if chant[-1] != " ":
+  print("The last character of chant is NOT a space")
+
+if chant[-1] == " ":
+  print("The last character of chant IS a space")
 
